48c851e079a59ab062321eb08effd4d73b77ff56
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2022-10-26 15:22:00 -0400

    chore: Add the processor.py file with all of the TODO markers.

---
+++ project/pylang/processor.py
@@ -0,0 +1,19 @@
+"""Perform data processing using cron jobs implemented in Python."""
+
+# TODO: Add all of the required imports
+
+
+# TODO: Implement the following features:
+#
+# 1) Delete all of the files in the data/ directory before execution
+# 2) Run a data generation task that will create audio file data and save it to a CSV file
+# 3) Run a data summarization task that will summarize audio file data and save it to a CSV file
+# 4) Parse the command-line arguments to the program and configure the data processor
+# 5) Display diagnostic information about all of the provided command-line arguments
+# 6) Display timestamped information about how the program runs the generation and summarization tasks
+# 7) Do not display a stack trace if a person types CTRL-c and exit with a zero exit code
+# 8) Produce a zero exit code in all cases in which the program runs to completion without error
+# 9) Produces a non-zero exit code only in cases when, for instance, it experiences corrupted files
+# 10) Runs correctly in a virtual environment called venv that contains all of its dependencies
+
+# TODO: review the content in the README.md file for additional details about this project
